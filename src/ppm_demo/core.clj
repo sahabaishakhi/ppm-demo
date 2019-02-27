157cfb53bd614ccd8a105be154d7b564b71a426d
sahabaishakhi <baishakhi@front.live>
2019-02-27 12:56:05 +0530

    commit to change ppm

--- src/ppm_demo/core.clj
+++ src/ppm_demo/core.clj
@@ -1,9 +1,9 @@
 (ns ppm-demo.core)
 
 (def ppm_header (str "P6\n" "2 " "2\n" "255\n"
-                     "255, 0, 0"
-                     "0, 255, 255"
-                     "0, 0, 255"
+                     "255, 0, 0\n"
+                     "0, 255, 255\n"
+                     "0, 0, 255\n"
                      "255, 0, 255"))
 
 (spit "demoimg.ppm" ppm_header)
