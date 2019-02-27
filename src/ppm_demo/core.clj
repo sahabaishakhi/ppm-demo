20971dd3b563467179c8f9bbc35c9045a0eac9aa
sahabaishakhi <baishakhi@front.live>
2019-02-27 16:53:25 +0530

    grid commit

--- src/ppm_demo/core.clj
+++ src/ppm_demo/core.clj
@@ -6,4 +6,14 @@
                      "0,0,255\n"
                      "255,0,255"))
 
+(def height 400)
+(def width 600)
+(def grids (* width height))
+
+(def head (str "P3\n" height " " width "\n"))
+(def image-data (str head
+                     (clojure.string/join "\n" (repeat grids "255,0,0"))))
+(def ppm-content (str head image-data))
+
 (spit "demoimg.ppm" ppm_header)
+(spit "griddemo.ppm" ppm-content)
