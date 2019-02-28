ed897c3c3dae656f8ebacf72eb6f46188563cc8c
sahabaishakhi <baishakhi@front.live>
2019-02-28 17:09:23 +0530

    comm

--- src/ppm_demo/core.clj
+++ src/ppm_demo/core.clj
@@ -4,7 +4,7 @@
 (def width 6)
 (def grids (* width height))
 
-(def head (str "P3\n" height " " width "255" "\n" ))
+(def head (str "P3\n" height " " width " 255" "\n" ))
 (def image-data (str head
                      (clojure.string/join "\n" (repeat grids "255 0 0"))))
 (def ppm-content image-data)
