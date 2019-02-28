e289b6ad539d4fb94230a9a436f08fe68dd669ce
sahabaishakhi <baishakhi@front.live>
2019-02-28 15:54:59 +0530

    rgb val test

--- src/ppm_demo/core.clj
+++ src/ppm_demo/core.clj
@@ -1,12 +1,12 @@
 (ns ppm-demo.core)
 
-(def height 400)
-(def width 600)
+(def height 4)
+(def width 6)
 (def grids (* width height))
 
 (def head (str "P3\n" height " " width "\n"))
 (def image-data (str head
-                     (clojure.string/join "\n" (repeat grids "255,0,0"))))
+                     (clojure.string/join "\n" (repeat grids "255 0 0"))))
 (def ppm-content image-data)
 
 (spit "griddemo.ppm" ppm-content)
